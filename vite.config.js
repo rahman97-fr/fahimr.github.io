1f9b3c56ce9d0e77ce204055bb8fb4a1053fc6f7
fahim rahman <fahimrahman@fahims-MacBook-Air.local>
2025-01-07 15:00:14 -0500

    Save local changes before pulling

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/fahimr.github.io/', // Add your repository name here
   plugins: [react()],
-})
+});
